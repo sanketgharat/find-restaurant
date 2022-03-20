359966d94677056e5212fcd4c75eea9a0a072f65
sanketgharat <sanketgharat22@gmail.com>
2022-03-20 19:40:33 +0530

    bug fix

--- app/src/main/java/com/sg/findrestaurant/utils/Constants.kt
+++ app/src/main/java/com/sg/findrestaurant/utils/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val BASE_URL = "https://api.yelp.com/v3/"
     const val DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss"
     const val DISTANCE_M_PER_KM = 1000
-    const val API_TOKEN = "Bearer XPFgzKwZGK1yqRxHi0d5xsARFOLpXIvccQj5jekqTnysweGyoIfVUHcH2tPfGq5Oc9kwKHPkcOjk2d1Xobn7aTjOFeop8x41IUfVvg2Y27KiINjYPADcE7Qza0RkX3Yx"
+    const val API_TOKEN = "put api token here"
     const val DEFAULT_LOCATION = "NYC"
 
 }
